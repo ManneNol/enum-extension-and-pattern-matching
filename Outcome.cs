bfd0131d1daf7c4a2344a9019c3904d2f633f06b
ManneNol <manuel@nodehill.com>
2023-09-28 15:42:17 +0200

    initial commit

---
+++ Outcome.cs
@@ -0,0 +1,8 @@
+namespace enum_extension_and_pattern_matching;
+
+public enum Outcome
+{
+    Win,
+    Loss,
+    Draw,
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using enum_extension_and_pattern_matching;
+// enum extension
+// pattern matching
+
+Move myMove = Move.Paper;
+Move otherMove = Move.Rock;
+
+
+Outcome result = myMove.CheckOutcome(otherMove);
+Outcome result2 = otherMove.CheckOutcome(otherMove);
+int newpoint = myMove.OffsetPoints(8);
+
+Console.WriteLine(result);
+Console.WriteLine(myMove.GetPoints());
+Console.WriteLine(otherMove.GetPoints());
